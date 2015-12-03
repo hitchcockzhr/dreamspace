d32b163a0acfd76dbd19f12f76476fbe9d5ed898
Nicholas Iebba <gamenut908@verizon.net>
2015-12-03 18:46:12 -0500

    User-made Message progress
    Need to fully merge MVC Project

--- routes/views/createMessage.js
+++ routes/views/createMessage.js
@@ -6,7 +6,7 @@ module.exports = function(req, res) {
   var view = new keystone.View(req, res);
   var locals = res.locals;
 
- locals.section = 'me';
+ locals.section = 'me'; //whatever we decide the post page to look like (no longer Gallery)
  locals.title = 'Leave a message';
 
  view.on('message', { action: 'create-message' }, function(next) {
@@ -17,6 +17,12 @@ module.exports = function(req, res) {
        publishedDate: new Date()
      });
 
+     //keystone needs an updateHandler to be called on objects
+     //made outside of AdminUI
+     //updateHandler usually makes a predefined message, but here, one is specified
+     var updater = newMessage.getUpdateHandler(req, res, {
+       errorMessage: 'There was an error creating your new message';
+     });
 
    // automatically pubish posts by admin user
    if (locals.user.isAdmin) {
@@ -26,12 +32,28 @@ module.exports = function(req, res) {
    //calling the notify admin function did not seem to work here
    //might get rid of it if we can't implement it
 
-   /*newMessage.notifyAdmins();
-   req.flash('success', 'Your post has been added' + ((newMessage.state == 'draft') ? ' and will appear on the site once it\'s been approved' : '') + '.');
-   return res.redirect(page to redirect to, like /maker);*/
+
+   //Allows for custom UI on message
+   //https://gist.github.com/wuhaixing/e90b8497f925ff9c7bfc
+   updater.process(req.body, {
+     flashErrors: true,
+     logErrors: true,
+     fields: 'title, image, content'
+   }, function(err) {
+     if (err) {
+       locals.validationErrors = err.errors;
+     } else {
+       newMessage.notifyAdmins();
+       req.flash('success', 'Your message has been added' + ((newMessage.state == 'published')) + '.');
+       return res.redirect('/'); //go home for now
+     }
+     next();
+   });
+
 
  });
 
+//like the maker page in domo maker
  view.render('createMessage');
 
 }
