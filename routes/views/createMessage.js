9fec61bf1efd814112db17c433e22d607601f8ef
Nicholas Iebba <gamenut908@verizon.net>
2015-12-06 18:00:19 -0500

    Code separation (Create, Delete, view Message)
    It's coming slow, but it's coming

--- routes/views/createMessage.js
+++ routes/views/createMessage.js
@@ -1,4 +1,6 @@
 var keystone = require('keystone');
+var _ = require('underscore');
+var User = keystone.list('User');
 var Message = keystone.list('Message');
 
 module.exports = function(req, res) {
@@ -7,50 +9,18 @@ module.exports = function(req, res) {
   var locals = res.locals;
 
 //made page to leave posts on
- locals.section = 'forum';
+ locals.section = 'board';
  locals.title = 'Leave a message';
 
- view.on('message', { action: 'create-message' }, function(next) {
+ // Load all messages
+ view.on('init', function(next) {
 
-   // handle form
-   var newMessage = new Message.model({
-       author: locals.user.id,
-       publishedDate: new Date()
-     });
+   var q = keystone.list('Message').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('10');
 
-     //keystone needs an updateHandler to be called on objects
-     //made outside of AdminUI
-     //updateHandler usually makes a predefined message, but here, one is specified
-     var updater = newMessage.getUpdateHandler(req, res, {
-       errorMessage: 'There was an error creating your new message';
-     });
-
-   // automatically pubish posts by admin user
-   if (locals.user.isAdmin) {
-     newMessage.state = 'published';
-   }
-
-   //calling the notify admin function did not seem to work here
-   //might get rid of it if we can't implement it
-
-
-   //Allows for custom UI on message
-   //https://gist.github.com/wuhaixing/e90b8497f925ff9c7bfc
-   updater.process(req.body, {
-     flashErrors: true,
-     logErrors: true,
-     fields: 'title, image, content'
-   }, function(err) {
-     if (err) {
-       locals.validationErrors = err.errors;
-     } else {
-       newMessage.notifyAdmins();
-       req.flash('success', 'Your message has been added' + ((newMessage.state == 'published')) + '.');
-       return res.redirect('/'); //go home for now
-     }
-     next();
+   q.exec(function(err, results) {
+     locals.data.messages = results;
+     next(err);
    });
-
 
  });
 
@@ -86,13 +56,13 @@ module.exports = function(req, res) {
       return res.err(err);
     }
     req.flash('success', 'Your topic has been deleted.');
-		return res.redirect(req.user.url);
+		return res.redirect(req.user.url); //check this line
   });
 
 
  });
 
 //like the maker page in domo maker
- view.render('createMessage');
+ view.render('message');
 
 }
---
+++ routes/views/newMessage.js
@@ -0,0 +1,63 @@
+//new message form
+var keystone = require('keystone');
+var _ = require('underscore');
+var Message = keystone.list('Message');
+
+module.exports = function(req, res) {
+
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+
+  //made page to leave posts on
+  locals.section = 'new-message'; //check this
+  locals.title = 'Leave a message';
+
+  locals.form = req.body;
+
+  view.on('message', { action: 'create-message' }, function(next) {
+
+   // handle form
+   var newMessage = new Message.model({
+       author: locals.user.id,
+       publishedDate: new Date()
+     });
+
+     //keystone needs an updateHandler to be called on objects
+     //made outside of AdminUI
+     //updateHandler usually makes a predefined message, but here, one is specified
+     var updater = newMessage.getUpdateHandler(req, res, {
+       errorMessage: 'There was an error creating your new message';
+     });
+
+   // automatically pubish posts by admin user
+   if (locals.user.isAdmin) {
+     newMessage.state = 'published';
+   }
+
+   //calling the notify admin function did not seem to work here
+   //might get rid of it if we can't implement it
+
+
+   //Allows for custom UI on message
+   //https://gist.github.com/wuhaixing/e90b8497f925ff9c7bfc
+   updater.process(req.body, {
+     flashErrors: true,
+     logErrors: true,
+     fields: 'title, image, content'
+   }, function(err) {
+     if (err) {
+       locals.validationErrors = err.errors;
+     } else {
+       newMessage.notifyAdmins();
+       req.flash('success', 'Your message has been added' + ((newMessage.state == 'published')) + '.');
+       return res.redirect('/message/' + newMessage.key);
+     }
+
+   });
+
+
+  });
+
+  view.render('new-message');
+
+};
